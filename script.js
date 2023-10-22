2c66e7796cd1cf5170b4c84f65b9617a7eff327f
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-22 22:29:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const statusElement = document.getElementById("status");
-    const enterButton = document.getElementById("enterBtn");
-
-    enterButton.addEventListener("click", function () {
-        statusElement.innerHTML = '<hi>Entered Metaverse</hi>';
-    });
-});
+//your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+let ptag = document.getElementById("status");
+function changeText(){
+	let h1tag = document.createElement("h1");
+	h1tag.innerText="Entered Metaverse";
+	ptag.appendChild(h1tag);
+}
